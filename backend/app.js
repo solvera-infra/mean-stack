1976dc6be08a250b4d1b89f74d7c3d19beb88516
dwikiarlan <arrlancore@gmail.com>
2020-02-01 16:10:56 +0700

    add express route

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/posts", (req, res, next) => {
   const posts = [
     {
@@ -16,7 +29,7 @@ app.use("/api/posts", (req, res, next) => {
     }
   ];
   res.status(200).json({
-    message: 'Posts fetched succesfully!',
+    message: "Posts fetched succesfully!",
     posts: posts
   });
 });
