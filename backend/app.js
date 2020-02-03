b50bc934526d3c09f03908f3945b601808b4bef1
Riza Masta Sputra <rizamasta@gmail.com>
2020-02-03 22:30:05 +0700

    update package to  section4/1-added-mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Post = require('./models/post');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -18,9 +20,12 @@ app.use((req, res, next) => {
   );
   next();
 });
-
+// QuBqs0T45GDKPlIG
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
