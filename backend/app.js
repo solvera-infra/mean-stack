40aee9108b5d4b2e4b4a52916886dc341ebbd153
Riza Masta Sputra <rizamasta@gmail.com>
2020-02-03 22:49:08 +0700

    update url

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 mongoose
   .connect(
-    "mongodb+srv://max:QuBqs0T45GDKPlIG@cluster0-ntrwp.mongodb.net/node-angular?retryWrites=true"
+    "url"
   )
   .then(() => {
     console.log("Connected to database!");
