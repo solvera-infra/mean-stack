ea144ea83f522c0fcdf4a749f4287776701abae6
Riza Masta Sputra <rizamasta@gmail.com>
2020-02-04 17:01:58 +0700

    fixing warning

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,32 +1,33 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Subject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { Post } from "./post.model";
+import { Post } from './post.model';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getPosts() {
     this.http
       .get<{ message: string; posts: any }>(
-        "http://localhost:3000/api/posts"
+        'http://localhost:3000/api/posts'
       )
       .pipe(map((postData) => {
-        return postData.posts.map(post => {
+        return postData.posts.map(val => {
           return {
-            title: post.title,
-            content: post.content,
-            id: post._id
+            title: val.title,
+            content: val.content,
+            id: val._id
           };
         });
       }))
       .subscribe(transformedPosts => {
+        console.log(transformedPosts);
         this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
@@ -39,7 +40,7 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string }>("http://localhost:3000/api/posts", post)
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
       .subscribe(responseData => {
         console.log(responseData.message);
         this.posts.push(post);
@@ -48,7 +49,7 @@ export class PostsService {
   }
 
   deletePost(postId: string) {
-    this.http.delete("http://localhost:3000/api/posts/" + postId)
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
       .subscribe(() => {
         console.log('Deleted!');
       });
