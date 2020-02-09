6385dab1545783652e85b9285244f5a6882595e1
Riza Masta Sputra <rizamasta@gmail.com>
2020-02-09 21:49:47 +0700

    fix connection

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 mongoose
   .connect(
-    "mongodb+srv://max:QuBqs0T45GDKPlIG@cluster0-ntrwp.mongodb.net/node-angular?retryWrites=true"
+    "mongodb+srv://mdb:tJc8QwpBmsUuZxpX@cluster0-zrsxe.mongodb.net/test?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected to database!");
