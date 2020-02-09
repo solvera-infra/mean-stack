736a6d313d3d13cdd4d310ddd102d5fb5540923e
Riza Masta Sputra <rizamasta@gmail.com>
2020-02-09 22:25:04 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 mongoose
   .connect(
-    "mongodb+srv://max:QuBqs0T45GDKPlIG@cluster0-ntrwp.mongodb.net/node-angular?retryWrites=true"
+    "mongodb+srv://mdb:tJc8QwpBmsUuZxpX@cluster0-zrsxe.mongodb.net/test?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected to database!");
